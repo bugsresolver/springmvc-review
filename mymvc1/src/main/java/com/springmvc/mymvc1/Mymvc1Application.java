763e4c9225119bbed62863f567614cee6b7df922
aaa <aaa@email.com>
2022-01-04 10:27:25 -0700

    first submit

---
+++ mymvc1/src/main/java/com/springmvc/mymvc1/Mymvc1Application.java
@@ -0,0 +1,13 @@
+package com.springmvc.mymvc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mymvc1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mymvc1Application.class, args);
+    }
+
+}
